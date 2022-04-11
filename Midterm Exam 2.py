82533cb1c777992682ee8a640457868b324b3659
Jeralph-Red <99183737+Jeralph-Red@users.noreply.github.com>
2022-04-11 11:40:35 +0800

    Update Midterm Exam 2.py

--- Midterm Exam 2.py
+++ Midterm Exam 2.py
@@ -1,19 +1,23 @@
 from tkinter import *
 from tkinter import ttk
 window = Tk()
+
+def Name():
+    txtfield['text'] = 'gg'
+
 window.geometry("500x250+10+20")
 window.title("Midterm in OOP")
 
 label = Label(window, text="Enter your fullname: ", fg="red")
 label.place(x=60, y=70)
 
-txtfield = Entry(window, bd=7)
+txtfield = Entry(window, textvariable='gg', bd=7)
 txtfield.place(x=270, y=65)
 
-button = Button(window, text="Click to display your Fullname", fg="red")
+button = Button(window, text="Click to display your Fullname", fg="red", command=Name)
 button.place(x=60, y=140)
 
-txtfield = Entry(window, bd=7)
+txtfield = Entry(window, text = 'Name', bd=7)
 txtfield.place(x=270, y=140)
 
 window.mainloop()
